cbf172c05816da183bf2524269d854a2c2caab08
talel <talelmejri8@gmail.com>
2022-08-17 14:58:54 +0100

    class add book

---
+++ classes/book_manager.php
@@ -0,0 +1,16 @@
+
+ <?php 
+    require_once "db_connected.php";
+
+    class book{
+        private $pdo;
+         public function __construct(){
+            $this->pdo=new database();
+         }
+
+         public function add_book(String $namee){
+          
+         }
+    }
+
+?>
