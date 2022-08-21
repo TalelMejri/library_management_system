0ea161c937e29784b4294f13d8eb2bd68be5730d
talel <talelmejri8@gmail.com>
2022-08-21 17:14:33 +0100

    design log in

--- log_out.php
+++ log_out.php
@@ -1,6 +1,10 @@
 <?php
-
+  
     session_start();
+    if(!isset($_SESSION['name'])){
+        header('location:login.php');
+        exit;
+    }
     session_destroy();
     unset($_SESSION['name']);
     unset($_SESSION['user_name']);
