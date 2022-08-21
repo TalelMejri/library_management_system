3505a0f084648cc99eecf793a769fbb4c59adf27
talel <talelmejri8@gmail.com>
2022-08-21 14:30:25 +0100

    add side bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-
- window.addEventListener('scroll',function(){
-    const header=document.querySelector('nav');
-    header.classList.toggle('sticky',window.scrollY>0);
- })
-
+let btn=document.querySelector('#sidebarcollapse');
+let side_bar=document.querySelector('#sidebar');
+let content=document.querySelector('#content');
+btn.addEventListener('click',function(){
+   side_bar.classList.toggle('active');
+   content.classList.toggle('active');
+})
