89d33d41b867ee622809fc1f9c5fd8780ced4ba9
talel <talelmejri8@gmail.com>
2022-08-22 13:46:09 +0100

    fix overview

--- profil_admin.php
+++ profil_admin.php
@@ -11,6 +11,14 @@
     $number_book_enable=$a->nombre_books_enabel_or_not($enable);
     $not_enable='not_enable';
     $number_book_not_enable=$a->nombre_books_enabel_or_not($not_enable);
+    if($number_all_book>0){
+    $pourcentage_book_issue= round( $number_book_not_enable*100/$number_all_book,2);
+    $pourcentage_book_enable=round(100-$pourcentage_book_issue,2);
+    }else{
+        $pourcentage_book_issue=0;
+        $pourcentage_book_enable=0;
+    }
+
     $page_titel="Accueil";
     $show=null;
     $template="profil_admin";
