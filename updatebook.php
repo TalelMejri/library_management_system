af02fe96694f129b0111fe94a418a497ad332dce
talel <talelmejri8@gmail.com>
2022-08-24 18:17:08 +0100

    fix some design

--- updatebook.php
+++ updatebook.php
@@ -4,20 +4,31 @@
         header("location:login.php");
         exit;
     }
+
+     if(!(array_key_exists('id',$_GET) or array_key_exists('id',$_POST)) and !(ctype_digit($_GET['id']) or ctype_digit($_POST['id']))){
+         header("location:consultebook.php");
+        exit();
+    } 
+
         include "./classes/File.php";
         include "./classes/book_manager.php";
-        $book=new book();
+        
+    $book=new book();  
+    $errors=[];  
     if(isset($_POST['update'])){
-     
+        var_dump("dddsd");
+        exit;
      extract($_POST);
      $file=new File('./storage/book/',$_FILES['avatar']);
      $avatar="./storage/book/".$file->getfilename();
+     
      if(empty($errors)){
-         $new=$book->updatebook($name_book,$author,$description,$nbr,$avatar,$idbook);
+        $new=$book->updatebook($name_book,$author,$description,$nbr,$avatar,$idbook);
         header("location:consultebook.php");
         exit;
     }
 }
+
     $books=$book->getbookbyid(array_key_exists('id',$_GET) ? $_GET['id'] : $_POST['id']);
     show_form:
     $show=null;
