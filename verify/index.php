b7b97df350dd1ec386c9864789bcb077d8402508
talel <talelmejri8@gmail.com>
2022-08-30 13:29:31 +0100

    ajouter affichage de verification et errors

--- verify/index.php
+++ verify/index.php
@@ -5,28 +5,31 @@ include "../classes/admin_user.php";
 $email = $_GET["key"];
 $token = $_GET["token"];
 $admin=new user();
+$errors=[];
 if(!$email || !$token){
     echo "Email or token missing";
     exit;
 }else{
     $result = $admin->validateEmail($email,$token);
     if(!$result){
-        echo "User doesn't exist";
-        exit;
+        $errors[0]="User doesn't exist";
+        goto show;
     }else if ($result == 1){
-        echo "jawna behi";
+        header("location:../login?verfiy=Your account has been verified avec succefully&color=success");
         exit;
     }
     else if ($result == 2 ){
-        echo "Your account is already verified";
-        exit;
+        $errors[0]="Your account is already verified";
+        goto show;
     }else if ($result == 3){
-        echo "Something went wrong";
-        exit;
+        $errors[0]="Something went wrong";
+        goto show;
     }else if ($result == 4){
-        echo "Token doesn't match email adress";
-        exit;
+        $errors[0]="Token doesn't match email adress";
+        goto show;
     }
+    show:
+    include 'verify.phtml';
 }
 
 
