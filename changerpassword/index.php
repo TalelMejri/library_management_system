37d6235d21ff4bbcb73401db4ba5f17f981b7973
talel <talelmejri8@gmail.com>
2022-08-31 13:51:07 +0100

    changer password

--- changerpassword/index.php
+++ changerpassword/index.php
@@ -2,11 +2,34 @@
 
  include "../classes/admin_user.php";
  $admin=new user();
- $token=$_GET['token_check'];
-        $password=$_POST['password'];
-        $admin->changerpassword($token,$password);
-        header("location:../login");
-        exit;
- include "./changerpassword.phtml";
+ $suivant=1;
+
+ if(isset($_POST['confirmer'])){  
+       $suivant=0; 
+       extract($_POST);
+       $user = new User();
+       $verify = $user->checkToken($email,$code);
+       $id=$verify['id'];
+       if ($verify){
+              $suivant=0; 
+       }else{
+           echo "Please check the code or the email";
+           exit;
+       }
+    }
+
+    if(isset($_POST['update'])){
+           $suivant=0;
+           extract($_POST);
+           $admin->changerpassword($id,$password);
+           header("location:../login");
+           exit;
+       }
+      
+$template ="changerpassword";
+$page_titel = "Change password";
+$show = false;
+include "../auth_layout.phtml";
+//  include "./changerpassword.phtml";
 
 ?>
