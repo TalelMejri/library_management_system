d862e75825d81027f2bad378b7c2ffaffd16a133
talel <talelmejri8@gmail.com>
2022-08-31 16:07:11 +0100

    log out user

--- log_out.php
+++ log_out.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
-    if(!isset($_SESSION['name'])){
+    if(!isset($_SESSION['name']) && !isset($_SESSION['username'])){
         header('location:../login');
         exit;
     }
     session_destroy();
     unset($_SESSION['name']);
-    unset($_SESSION['user_name']);
+    unset($_SESSION['username']);
     header ('location:./login');
 ?>
