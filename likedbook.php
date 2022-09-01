a41a7879cb5104de7a49468f7a0ecdcec9435afa
talel <talelmejri8@gmail.com>
2022-09-01 15:54:16 +0100

    liked book

---
+++ likedbook.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+include "./classes/admin_user.php";
+$admin=new user();
+if(isset($_POST['like'])){
+    extract($_POST);
+    $admin->AddLikedBook($_SESSION['userid'],$idbook);
+  
+}
+header("location:./profiluser");
+exit;
+?>
