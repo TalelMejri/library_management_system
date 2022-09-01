3da3f0b73f33880d0e66f558c443bf81ea8ca37d
talel <talelmejri8@gmail.com>
2022-09-01 16:55:11 +0100

    add nbr liked

--- profiluser/index.php
+++ profiluser/index.php
@@ -4,12 +4,12 @@
       header("location:../layout.phtml");
       exit;
   }
-
+  include "../classes/admin_user.php";
   include "../classes/book_manager.php";
-
+  $admin=new user();
   $book=new book();
   $books=$book->getnewbok();
-
+ 
   $page_titel="Dashbord";
   $show=null;
   $template="profiluser";
