6a0050a1c6a600a5e283d623d46ba944f82512da
talel <talelmejri8@gmail.com>
2022-09-01 15:54:10 +0100

    design profil user

--- profiluser/index.php
+++ profiluser/index.php
@@ -1,9 +1,14 @@
 <?php 
   session_start();
-  if(empty($_SESSION['userid'])){
+  if(!isset($_SESSION['userid'])){
       header("location:../layout.phtml");
       exit;
   }
+
+  include "../classes/book_manager.php";
+
+  $book=new book();
+  $books=$book->getnewbok();
 
   $page_titel="Dashbord";
   $show=null;
