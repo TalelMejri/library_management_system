e9aabf52b4fb8e1ebf97c57dbbb3568bc89909e8
talel <talelmejri8@gmail.com>
2022-09-04 13:54:14 +0100

    fix favorite book

--- listfavoritebook/index.php
+++ listfavoritebook/index.php
@@ -1,11 +1,13 @@
 <?php
 
     session_start();
+    include "../classes/admin_user.php";
     include "../classes/book_manager.php";
+    $admin=new user();
     $book=new book();
     $books_favorite=$book->getbooksfavorite($_SESSION['userid']);
     $template="listfavoritebook";
     $page_titel="favorite";
     $show=null;
-    include "../auth_layout.phtml";
+    include "../layout.phtml";
 ?>
