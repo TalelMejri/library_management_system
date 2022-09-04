83b85b24e68b775184a5e82c1707da7b13f0f8f9
talel <talelmejri8@gmail.com>
2022-09-04 13:54:24 +0100

    fix bugs

--- favoritebook.php
+++ favoritebook.php
@@ -7,6 +7,11 @@ if(isset($_POST['favorite'])){
     extract($_POST);
     $admin->AddfavoriteBook($_SESSION['userid'],$idbook);
 }
+if(isset($_POST['name_book'])){
+    header("location:./listfavoritebook");
+    exit;
+}else{
 header("location:./profiluser");
 exit;
+}
 ?>
