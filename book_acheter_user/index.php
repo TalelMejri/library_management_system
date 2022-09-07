938c45bf6e2ee891ac54963f945b46052acde966
talel <talelmejri8@gmail.com>
2022-09-07 10:54:35 +0100

    fix bugs

--- book_acheter_user/index.php
+++ book_acheter_user/index.php
@@ -3,7 +3,6 @@
    require_once ("../classes/classes.php");
    $book=new book();
    $commande=$book->getcommande_user($_SESSION['userid']);
-   
    $template="book_acheter_user";
    $page_titel="Book payer";
    $show=null;
