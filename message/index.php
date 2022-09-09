2d032ccc0d9f6e7f7a90a27e3e191b9a3b379df6
talel <talelmejri8@gmail.com>
2022-09-09 17:23:13 +0100

    design, box of chat

--- message/index.php
+++ message/index.php
@@ -2,9 +2,12 @@
 
 session_start();
 require_once "../classes/classes.php";
-
 $admin=new user();
 $all_user=$admin->get_user_chat();
+if(isset($_GET['id'])){
+ extract($_POST);
+ $user_choice=$admin->getuserbyid($_GET['id']);
+}
 $template="message";
 $page_titel="Chat";
 $show=null;
