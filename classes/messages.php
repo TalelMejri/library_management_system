fa4fa5ac92d53fc7cff07029b150774c75208426
talel <talelmejri8@gmail.com>
2022-09-09 19:44:44 +0100

    add request

--- classes/messages.php
+++ classes/messages.php
@@ -6,7 +6,16 @@
             $this->pdo=new database();
          }
 
-      
+       public function  send_message(int $idenvoi,int $idrecu,String $message){
+        $sql="INSERT INTO chat (idenvoi,idrecent,message) VALUES (:idenvoi,:idrecu,:message)";
+        $this->pdo->launch_query($sql,['idenvoi'=>$idenvoi,'idrecu'=>$idrecu,'message'=>$message]);
+       }
+
+       public function getmessageofuser(int $idenvoi,int $idrecu){
+        $sql="SELECT * from chat where idenvoi=:idsend and idrecent=:idrecoi or idenvoi=:idinverse and idrecent=:idreverse ";
+        $query=$this->pdo->launch_query($sql,['idsend'=>$idenvoi,'idrecoi'=>$idrecu,'idinverse'=>$idrecu,'idreverse'=>$idenvoi]);
+        return $query->fetchAll();
+       }
 
     }
 
