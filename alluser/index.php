c9b66df522d1fe211f5dba9c95981200d054f037
talel <talelmejri8@gmail.com>
2022-09-14 17:59:51 +0100

    delete white espace

--- alluser/index.php
+++ alluser/index.php
@@ -5,11 +5,10 @@
         exit;
     }
 
-
     require_once("../classes/classes.php");
     $user=new user();
     $page=isset($_GET['page']) ? $_GET['page'] : 1;
-    $limit=isset($_POST['record']) ? $_POST['record'] : 2;
+    $limit=isset($_POST['record']) ? $_POST['record'] :2 ;
     $next=$page < $user->countuser() /$limit ? $page+1 : 1;
     $previous= $page > 1 ? $page-1 : 1;
     $start=($page-1)* $limit;
