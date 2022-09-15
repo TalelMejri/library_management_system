ce377d2e775e6cdae4fbc2fba67db09d3cf5dc8e
talel <talelmejri8@gmail.com>
2022-09-15 16:54:12 +0100

    add search form

--- alluser/index.php
+++ alluser/index.php
@@ -8,12 +8,17 @@
     require_once("../classes/classes.php");
     $user=new user();
     $page=isset($_GET['page']) ? $_GET['page'] : 1;
-    $limit=isset($_POST['record']) ? $_POST['record'] :2 ;
+    $limit=isset($_GET['record']) ? $_GET['record'] :2 ;
     $next=$page < $user->countuser() /$limit ? $page+1 : 1;
     $previous= $page > 1 ? $page-1 : 1;
     $start=($page-1)* $limit;
     $alluser=$user->getalluser($limit,$start);
     $pages=ceil($user->countuser()/$limit);
+    
+    if(isset($_POST['btn_search'])){
+        extract($_POST);
+        $alluser=$user->getbyname($search);
+    }
     
     $show=null;
     $template="alluser";
