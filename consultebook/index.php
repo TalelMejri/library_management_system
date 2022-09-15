2500805abf1168ca28c37e8968f0c9dbf52f71cf
talel <talelmejri8@gmail.com>
2022-09-15 17:09:04 +0100

    add search book

--- consultebook/index.php
+++ consultebook/index.php
@@ -1,16 +1,21 @@
 <?php 
     session_start();
     if(!isset($_SESSION['name'])){
-     header("location:../login");
-     exit;
- }
+       header("location:../login");
+       exit;
+     }
     require_once("../classes/classes.php");
      $book=new book();
      if(isset($_POST['delete'])){
           $book->deleteallbook();
           echo "<script>alert('delete done')</script>";
      }
+
      $books=$book->get_all();
+     if(isset($_POST['btn_search'])){
+          extract($_POST);
+          $books=$book->searchforbook($search);
+     }
      $template="consultebook";
      $show=null;
      $page_titel="all book";
