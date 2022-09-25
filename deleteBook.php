5521a2a40f76f51739c2ca1d407cdb96b3f3fdb8
talel <talelmejri8@gmail.com>
2022-09-25 10:31:44 +0100

    condition in deletbook

--- deleteBook.php
+++ deleteBook.php
@@ -4,6 +4,10 @@
         header("location:../login");
         exit;
     }
+    if(!array_key_exists('id',$_GET)){
+        header("location:../login");
+        exit;
+       }
     require_once("./classes/classes.php");
     $delete_book=new book();
     $delete_book->delete_book($_GET['id']);
